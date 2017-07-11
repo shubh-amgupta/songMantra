0eedcb65212fec76365c0cd8c45e2f2d340c5da3
Shubham Gupta <shubhamg.0907@gmail.com>
2017-07-11 20:26:54 +0530

    Added Player functionality and Volume Slider

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $("#signup_submit").on('click', function() {
    var name = $("#email_signup").val();
    if(name.length > 2) {
          // window.location.href="user_page.html"; // To go to a new page user_page.html via button click, can be used in future for signup, etc
-         // return false; // To stop sbsorbtion of events and load the page important!
+         // return false; // To stop absorbtion of events and load the page important!
          $("#user_name").text(name);
          $("#user").addClass('hidden');
          $("#display").removeClass('hidden');
@@ -14,14 +14,12 @@ $("#signup_submit").on('click', function() {
    }
 });
 
-var currentSongNumber = 1;
-var willLoop = 0;
-var willShuffle = 0;
 
 
-// Display user's name in right side of the header
-//$("user_name").html($("email_signup").val()); Dynamic
-//$("#user_name").text("Shubham");
+var currentSongNumber = 1; //Used in different functions to check surrent Song Number
+var willLoop = 0; //To toggle loop function in player
+var willShuffle = 0; //To toggle shuffle function in toggle
+
 
 
 // Play and Pause action
@@ -31,14 +29,8 @@ $('.play-icon').on('click', function() {
 });
 
 
-// Play and Pause on SpaceBar
-// $('body').on('keypress', function(event) {
-//             if (event.keyCode == 32) {
-//                 var song = document.querySelector('audio');
-//                 toggleSong();
-//             }
-//         });
 
+// On space-bar(keycode - 32) press Play & Pause song except if space in pressed in INPUT tag
 $('body').on('keypress', function(event) {
    var target = event.target;
    if (event.keyCode == 32 && target.tagName != 'INPUT') {
@@ -49,29 +41,23 @@ $('body').on('keypress', function(event) {
 
 
 
-
+// Function - to toggle song Play if Paused, Paused if Play
 function toggleSong() {
    var song = document.querySelector('audio');
-   timeJump();
    if(song.paused == true) {
-      console.log('Playing');
       $('.play-icon').removeClass('fa-play').addClass('fa-pause');
       song.play();
    }
    else {
-      console.log('Pausing');
       $('.play-icon').removeClass('fa-pause').addClass('fa-play');
       song.pause();
    }
 }
 
-// var songName1 = 'Tamma Song';
-// var songName2 = 'Humma Song';
-// var songName3 = 'Nashe Si Chadh Gayi';
-// var songName4 = 'The Breakup Song';
 
-var songList = ['Tamma Song', 'Humma Song', 'Nashe Si Chadh Gayi', 'The Breakup Song'];
 
+// Onload function performed wheb html document is fully loaded. Idealy all JS should be inside onload
+// Function - changeCurrentSongDetails chages the details of a song in object array index i
 window.onload = function() {
    changeCurrentSongDetails(songs[0]);
    for(var i =0; i < songs.length;i++) {
@@ -87,44 +73,16 @@ window.onload = function() {
    setInterval(function() {
    updateCurrentTime();
    },1000);
+   setInterval(function() {
+   updateSongProgress();
+   },1000);
+   var audio = document.querySelector('audio');
+   var volume = audio.volume;
+   setVolume(volume);
 }
 
 
-var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
-//
-// $('#song1').click(function() {
-//    var audio = document.querySelector('audio');
-//    var currentSong = audio.src;
-//    if(currentSong.search(fileNames[0]) != -1)
-//    {
-//    toggleSong();
-//    }
-//    else {
-//    audio.src = fileNames[0];
-//    toggleSong();
-//    }
-// });
-// $('#song2').click(function() {
-//    var audio = document.querySelector('audio');
-//    audio.src = fileNames[1];
-//    toggleSong();
-// });
-//
-// $('#song3').click(function() {
-//    var audio = document.querySelector('audio');
-//    audio.src = fileNames[2];
-//    toggleSong();
-// });
-//
-//
-// $('#song4').click(function() {
-//    var audio = document.querySelector('audio');
-//    audio.src = fileNames[3];
-//    toggleSong();
-// });
-
-
-//
+// Function - updateCurrentTime updates the current song time elapsed and total
 function updateCurrentTime() {
     var song = document.querySelector('audio');
     var currentTime = Math.floor(song.currentTime);
@@ -136,7 +94,8 @@ function updateCurrentTime() {
 }
 
 
-//
+
+// Function -  it is a fancy time format to display song time
 function fancyTimeFormat(time)
 {
     // Hours, minutes and seconds
@@ -157,16 +116,12 @@ function fancyTimeFormat(time)
 }
 
 
-// //
-// for (var i = 0; i < fileNames.length ; i++) {
-//     addSongNameClickEvent(fileNames[i],i+1)
-// }
 
-
-//
+// Function - addSongNameClickEvent adds all the song in a table list and plays song on click
 function addSongNameClickEvent(songObj,position) {
    var songName = songObj.fileName;
     var id = '#song' + position;
+    console.log(currentSongNumber);
     $(id).click(function() {
       var audio = document.querySelector('audio');
       var currentSong = audio.src;
@@ -182,7 +137,8 @@ function addSongNameClickEvent(songObj,position) {
 }
 
 
-//
+
+// Object Array - Songs which contain all the songs with there info
 var songs = [{
         'name': 'Tamma Song',
         'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
@@ -214,9 +170,11 @@ var songs = [{
         'duration': '2:29',
         'fileName': 'song4.mp3',
        'image' : 'song4.jpg'
-    }]
+}]
 
 
+
+// Function - changeCurrentSongDetails changes the details of the song like image, name, album
 function changeCurrentSongDetails(songObj) {
    $('.current-song-image').attr('src','img/' + songObj.image);
    $('.current-song-name').text(songObj.name);
@@ -225,46 +183,191 @@ function changeCurrentSongDetails(songObj) {
 }
 
 
+
+// Datatable function to run Data tables for searching and sorting songs by name, duration
 $(document).ready(function() {
    $('#songs').DataTable({
-      paging: false,
+      paging: false, //Paging false - doesn't displays pagenumber and list
    });
 });
 
 
 
+//
 $('#songs_filter input').attr('placeholder', "search");
 
 
 
+//Function - onclick of loop icon to loop songs
 $('.fa-repeat').on('click',function() {
     $('.fa-repeat').toggleClass('disabled')
     willLoop = 1 - willLoop;
 });
 
 
+// Function - onclick of shuffle icon to randomly play songs
 $('.fa-random').on('click',function() {
     $('.fa-random').toggleClass('disabled')
     willShuffle = 1 - willShuffle;
 });
 
+
+// Function - timeJump to end of 5 seconds of song
 function timeJump() {
     var song = document.querySelector('audio')
     song.currentTime = Math.floor(song.duration) - 5;
 };
 
 
+
+// Function - onEnd of a song to loop through songs and to shuffle songs
 $('audio').on('ended',function() {
     var audio = document.querySelector('audio');
-    if(currentSongNumber < 4) {
+    if (willShuffle == 1) {
+        var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
+        var nextSongObj = songs[nextSongNumber-1];
+        audio.src = nextSongObj.fileName;
+        toggleSong();
+        changeCurrentSongDetails(nextSongObj);
+        currentSongNumber = nextSongNumber;
+    }
+    else if(currentSongNumber < 4) {
         var nextSongObj = songs[currentSongNumber];
-        audio.src = nextSongObj.fileName; // Change Source
-        toggleSong(); // Play Next Song
-        changeCurrentSongDetails(nextSongObj); // Update Image
-        currentSongNumber = currentSongNumber + 1; // Change State
+        audio.src = nextSongObj.fileName;
+        toggleSong();
+        changeCurrentSongDetails(nextSongObj);
+        currentSongNumber = currentSongNumber + 1;
+    }
+    else if(willLoop == 1) {
+        var nextSongObj = songs[0];
+        audio.src = nextSongObj.fileName;
+        toggleSong();
+        changeCurrentSongDetails(nextSongObj);
+        currentSongNumber =  1;
     }
     else {
         $('.play-icon').removeClass('fa-pause').addClass('fa-play');
         audio.currentTime = 0;
     }
+})
+
+
+
+// Function - To return a random number within a given range except some number (exculded)
+function randomExcluded(min, max, excluded) {
+    var n = Math.floor(Math.random() * (max-min) + min);
+    if (n >= excluded) n++;
+    return n;
+}
+
+
+
+// Function - onClick on next icon to play next song
+$('.fa-step-forward').on('click', function() {
+    console.log("clicked next");
+    if(currentSongNumber < songs.length) {
+      nextSong();
+    }
+    if(currentSongNumber == 1) {
+      $('.fa-step-backward').removeClass('disabled');
+      nextSong();
+    }
+    if(currentSongNumber == songs.length) {
+      $('.fa-step-forward').addClass('disabled');
+    }
+});
+
+
+
+// Function - To play next Song
+function nextSong() {
+   console.log("next Song"+currentSongNumber);
+   var audio = document.querySelector('audio');
+   var nextSongObj = songs[currentSongNumber];
+   console.log(currentSongNumber);
+   audio.src = nextSongObj.fileName;
+   toggleSong();
+   changeCurrentSongDetails(nextSongObj);
+   currentSongNumber += 1;
+
+}
+
+
+
+// Function - onClick on back icon to play previous song
+$('.fa-step-backward').on('click', function() {
+    console.log("clicked back");
+    console.log(currentSongNumber);
+    if(currentSongNumber > 0) {
+      previousSong();
+    }
+    if(currentSongNumber == songs.length-1) {
+      $('.fa-step-forward').removeClass('disabled');
+      previousSong();
+    }
+    if(currentSongNumber == 0) {
+      $('.fa-step-backward').addClass('disabled');
+    }
+});
+
+
+
+// Function - To play previous song
+function previousSong() {
+   console.log("prev Song"+currentSongNumber);
+   var audio = document.querySelector('audio');
+   var nextSongObj = songs[currentSongNumber-1];
+   console.log(currentSongNumber);
+   audio.src = nextSongObj.fileName;
+   toggleSong();
+   changeCurrentSongDetails(nextSongObj);
+   currentSongNumber -= 1;
+}
+
+
+
+// Function -  to set volume at a given percentage
+function setVolume(percentage){
+  var audio = document.querySelector('audio');
+  audio.volume = percentage;
+
+  var percentageOfVolume = audio.volume;
+  var percentageOfVolumeSlider = 100 * percentageOfVolume;
+
+  $('.volume-filled').css("width", Math.round(percentageOfVolumeSlider) + "%");
+}
+
+//Set's new volume id based off of the click on the volume bar.
+// Function - To calculate where click event has occured
+$('.volume-progress').on('click', function setNewVolume(event){
+  var volumeSliderWidth = event.offsetX;
+  var percentage = volumeSliderWidth/120;
+  setVolume(percentage);
+});
+
+
+
+//Function - to update progressbar
+function updateSongProgress() {
+   var audio = document.querySelector('audio');
+   var ct = audio.currentTime;
+   var td = audio.duration;
+   var percent = (ct/td) * 100;
+   $('.progress-filled').css("width", percent + "%");
+}
+
+
+
+// Function - to calculate where click event has occured on progress bar
+$(".player-progress").on('click', function(event) {
+   var $this = $(this);
+   var width = event.pageX - $this.offset().left;
+   console.log(event.pageX);
+   console.log($this.offset().left);
+   var total = $this.width();
+   console.log(total);
+   var time = (width/total) * 100;
+   var audio = document.querySelector('audio');
+   audio.currentTime = (audio.duration * time)/100;
+   updateSongProgress();
 })
